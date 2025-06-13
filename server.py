8d465b65411dca27932dc3a5ae26f7451bd02f85
ichininja <vinayaksemail@gmail.com>
2025-06-13 02:51:34 -0400

    cors middleware

--- server.py
+++ server.py
@@ -11,12 +11,13 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 app = FastAPI()
 
 # CORS middleware
-app.add_middleware(
-    CORSMiddleware,allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "https://vinspolicy.github.io"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
 
 @app.on_event("startup")
 async def ensure_db_exists():
